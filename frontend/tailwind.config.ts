89799de944ac4310bdcdcc1e95c68e9715c0c75e
Sotiris Maipas <sotiris.maipas@gmail.com>
2025-08-18 19:54:33 +0300

    feat(UI): change default fonts to open sans

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -3,6 +3,9 @@ export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['"Open Sans"', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
       colors: {
         primary: 'var(--color-primary)',
         secondary: 'var(--color-secondary)',
